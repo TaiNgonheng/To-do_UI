c4049c1d7648edeab7cd06547fa641a57f90220b
TaiNgonheng <heng68807@gmail.com>
2024-11-08 13:20:13 +0700

    section

---
+++ todo_ui/src/components/FooterComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FooterComponent = () => {
+    return (
+        <div>
+            <footer className='footer'>
+                <p className='text-center'>Copyrights Reserved at 2024 by Tai Ngonheng</p>
+            </footer>
+        </div>
+    )
+}
+export default FooterComponent
---
+++ todo_ui/src/components/HeaderComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+    return (
+        <div>
+            <header className='header'>
+                 <h3>Task Management🗒🪅</h3>
+            </header>
+        </div>
+    )
+}
+export default HeaderComponent
