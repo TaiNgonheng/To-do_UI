2c1fe9f173ca26a5543f814a8c30ce08d7c317e4
TaiNgonheng <heng68807@gmail.com>
2024-11-08 13:19:53 +0700

    Route with url

--- todo_ui/src/App.jsx
+++ todo_ui/src/App.jsx
@@ -1,12 +1,29 @@
 import { useState } from 'react'
 import './App.css'
 import ListTodoComponent from "./components/ListTodoComponent.jsx";
+import HeaderComponent from "./components/HeaderComponent.jsx";
+import FooterComponent from "./components/FooterComponent.jsx";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import TodoComponent from "./components/TodoComponent.jsx";
 
 function App() {
 
   return (
     <>
-        <ListTodoComponent/>
+        <BrowserRouter>
+            <HeaderComponent/>
+            <Routes>
+                {/*http:localhost:3000/*/}
+                <Route path='/' element={<ListTodoComponent/>}></Route>
+                {/*http://http:localhost:3000/todos*/}
+                <Route path='/todos' element={<ListTodoComponent/>}></Route>
+                {/*http:localhost3000/add-todo*/}
+                <Route path='/addtodo' element={<TodoComponent/>}></Route>
+                {/*http://localhost:3000/updateTodo*/}
+                <Route path='/updatetodo/:id' element={<TodoComponent/>}></Route>
+            </Routes>
+            <FooterComponent/>
+        </BrowserRouter>
     </>
   )
 }
