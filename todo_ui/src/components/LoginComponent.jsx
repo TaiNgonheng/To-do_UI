7eb47234b1ba81fb34fdbf5b03cee213368a0da9
TaiNgonheng <heng68807@gmail.com>
2024-11-17 10:03:31 +0700

    Header config

--- todo_ui/src/components/LoginComponent.jsx
+++ todo_ui/src/components/LoginComponent.jsx
@@ -33,7 +33,7 @@ const LoginComponent = () => {
             <div className='col-md-6 offset-md-3'>
                 <div className='card'>
                     <div className='card-header'>
-                        <h2 className='text-center'>Login Form</h2>
+                        <h2 className='text-center'>Login</h2>
                     </div>
                     <div className='card-body'>
                         <form>
